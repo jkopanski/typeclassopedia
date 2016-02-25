b3d22cb8854b49f182189cdffa3352beaed92b5c
Jakub Kopanski <J.Kopanski@imio.pw.edu.pl>
2016-02-25 15:25:14 +0100

    Applicative excercise

--- Applicative.hs
+++ Applicative.hs
@@ -1,6 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
-module Functor where
 
+module Applicative where
+
+import Data.Functor
+import Prelude (Maybe (..), repeat, zipWith, ($))
+
+class Functor f => Applicative f where
+  pure :: a -> f a
+  infixl 4 <*>
+  (<*>) :: f (a -> b) -> f a -> f b
 {-
  - prove that
  -
@@ -29,3 +37,26 @@ module Functor where
  -
  -}
 
+instance Applicative Maybe where
+  pure = Just
+  Just f <*> Just a = Just (f a)
+  _ <*> _ = Nothing
+
+newtype ZipList a = ZipList { getZipList :: [a] }
+
+instance Functor ZipList where
+  fmap f (ZipList xs) = ZipList (f <$> xs)
+
+instance Applicative ZipList where
+  pure x = ZipList (repeat x)
+  (ZipList gs) <*> (ZipList xs) = ZipList (zipWith ($) gs xs)
+
+class Functor f => Monoidal f where
+  unit :: f ()
+  (**) :: f a -> f b -> f (a,b)
+
+pure' :: Monoidal f => a -> f a
+pure' a = fmap (\_ -> a) unit
+
+(<*>.) :: Monoidal f => f (a -> b) -> f a -> f b
+(<*>.) g f = fmap (\(h, x) -> h x) (g ** f)
