5ab6812136638418e3a252316bafe61f46d937e5
Jakub Kopański <jkopansk@gmail.com>
2016-02-28 02:01:10 +0100

    more Monads

--- Monad.hs
+++ Monad.hs
@@ -2,27 +2,28 @@
 
 module Monad where
 
-import Prelude ((.))
-import Data.List (map, concat)
-import Data.Functor
-import Data.Function (const)
-import Control.Applicative
-import Prelude (Maybe (..), repeat, zipWith, ($))
+import Prelude
+-- import Data.List (map, concat)
+-- import Data.Functor
+-- import Data.Function (const, id)
+-- import Control.Applicative
+import Control.Monad (join)
+-- import Prelude (Maybe (..), repeat, zipWith, ($))
 
-class Applicative m => Monad m where
-  return :: a -> m a
-  (>>=)  :: m a -> (a -> m b) -> m b
-  (>>)   :: m a -> m b -> m b
-  m >> n = m >>= \_ -> n
+-- class Applicative m => Monad m where
+--   return :: a -> m a
+--   (>>=)  :: m a -> (a -> m b) -> m b
+--   (>>)   :: m a -> m b -> m b
+--   m >> n = m >>= \_ -> n
 
-instance Monad [] where
-  return x = ([x])
-  xs >>= g = concat [ g x | x <- xs ]
+-- instance Monad [] where
+--   return x = ([x])
+--   xs >>= g = concat [ g x | x <- xs ]
 
-instance Monad ((->) e) where
-  return  = const
-  -- x >>= g = g . x
-  (>>=) x g y = g (x y) y
+-- instance Monad ((->) e) where
+--   return  = const
+--   -- x >>= g = g . x
+--   (>>=) x g y = g (x y) y
 
 data Free f a = Var a
               | Node (f (Free f a))
@@ -41,3 +42,25 @@ instance (Functor f) => Monad (Free f) where
   return = Var
   (Var a)  >>= g = g a
   (Node a) >>= g = Node $ fmap (>>= g) a
+
+bind' :: (Monad m, Functor m) => m a -> (a -> m b) -> m b
+bind' x k = join $ fmap k x
+
+join' :: (Monad m, Functor m) => m (m a) -> m a
+join' a = a >>= id
+
+fmap' :: (Monad m, Functor m) => (a -> b) -> m a -> m b
+fmap' g a = a >>= (\x -> return $ g x)
+
+-- let g >=> h = \x -> g x >>= h then
+
+-- return >=> g = \x -> return x >>= g
+--              = return x >>= g
+
+-- g >=> return = \x -> g x >>= return
+--              = g >>= return
+
+-- (g >=> h) >=> k = p >=> k = \x -> p x >>= k
+-- p = g >=> h = \y -> g y >>= h
+
+-- \x -> g x >>= h >>= k
